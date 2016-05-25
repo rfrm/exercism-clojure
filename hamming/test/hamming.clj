6150e7ef58591869bde0ec91b9046da741590cff
Robinson Rodriguez <rfrodriguez1992@gmail.com>
2016-05-24 22:18:05 -0500

    Use destructuring

--- hamming/test/hamming.clj
+++ hamming/test/hamming.clj
@@ -3,5 +3,5 @@
 (defn distance [s1 s2]
   (if (= (count s1) (count s2))
     (->> (map vector s1 s2)
-         (filter (fn [pair] (not= (first pair) (last pair))))
+         (filter (fn [[s1_char s2_char]] (not= s1_char s2_char)))
          count)))
